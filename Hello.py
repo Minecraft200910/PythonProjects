f67aafc5bb201a24514b203d9b834ee348a5ea79
Aronliang <97389865+Minecraft200910@users.noreply.github.com>
2022-01-23 13:12:17 +0000

    这是一个。。。

---
+++ Hello.py
@@ -0,0 +1,14 @@
+print("hello,users")
+import turtle
+import turtle
+
+#初始化窗口
+t=turtle.Pen()
+turtle.speed(999)
+turtle.onscreenclick(t.setpos)
+turtle.bgcolor("blue")
+t.pencolor("green")
+#点击海龟
+turtle.onscreenclick(t.setpos)
+t.pensize(99)
+
